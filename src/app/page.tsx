dc5cae0fece48cbcfb8fa95b3645a3c0a81408b1
Tobias <tobiasramossilva@gmail.com>
2024-03-12 13:01:08 -0300

    Fazendo uma estilização em quase todos os componentes, alterando cores, tamamhos de fontes e bordas.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
 "use client";
 import CardProduct from "./components/card/page";
-import Filter from "./components/filter/page";
 import Header from "./components/header/page";
-import Search from "./components/search/page";
 import "./globals.css";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <Filter />
-      <Search />
       <CardProduct />
     </>
   );
