719202aaf4c86f798d1593f46c643b34670849d5
Tobias <tobiasramossilva@gmail.com>
2024-03-18 14:11:49 -0300

    fazendo uma popover para ver detalhes sobre o produto

--- src/app/components/card/page.tsx
+++ src/app/components/card/page.tsx
@@ -1,10 +1,25 @@
 "use client";
 import styles from "./page.module.css";
 import { useStore } from "../../store";
-import { useEffect } from "react";
-import { Button, Carousel, Rate } from "antd";
+import { useEffect, useState } from "react";
+import { Button, Carousel, Modal, Rate } from "antd";
+import { Product } from "../interface/product";
+import Link from "antd/es/typography/Link";
 const CardProduct = () => {
   const { inc, getAllProducts, products, addToCart } = useStore();
+  const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = (product: any) => {
+    setSelectedProduct(product);
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
 
   useEffect(() => {
     getAllProducts();
@@ -42,6 +57,7 @@ const CardProduct = () => {
               :
               <Rate disabled value={Number(product.rating.toFixed(1))} />
             </div>
+            <span className={styles.stock}>Estoque: {product.stock}</span>
           </div>
           <Button
             onClick={() => addProductToCart(product)}
@@ -51,8 +67,65 @@ const CardProduct = () => {
           >
             Comprar
           </Button>
+          <Link className={styles.link} onClick={() => showModal(product)}>
+            Ver mais...
+          </Link>
         </div>
       ))}
+      <Modal
+        title={selectedProduct ? selectedProduct.title : ""}
+        open={isModalOpen}
+        onCancel={handleCancel}
+        closable={true}
+        footer={null}
+      >
+        {selectedProduct && (
+          <div>
+            <Carousel autoplay>
+              {selectedProduct.images.map((image, index) => (
+                <img
+                  key={index}
+                  src={image}
+                  alt={`Imagem ${index + 1}`}
+                  className={styles.contentStyle}
+                />
+              ))}
+            </Carousel>
+            <div>
+              <span className={styles.characteristics}>Preço: </span>
+              <span>
+                {" "}
+                {selectedProduct.price.toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                })}
+              </span>
+            </div>
+            <div>
+              <span className={styles.characteristics}>Descrição: </span>
+              <span>{selectedProduct.description}</span>
+            </div>
+            <div>
+              <span className={styles.characteristics}>Estoque: </span>
+              <span>{selectedProduct.stock}</span>
+            </div>
+            <div>
+              <span className={styles.characteristics}>Categoria: </span>
+              <span>{selectedProduct.category}</span>
+            </div>
+            <div>
+              <span className={styles.characteristics}>Marca: </span>
+              <span>{selectedProduct.brand}</span>
+            </div>
+            <div>
+              <span className={styles.characteristics}>Avaliação: </span>
+              <span>
+              <Rate disabled value={Number(selectedProduct.rating.toFixed(1))} />
+            </span>
+            </div>
+          </div>
+        )}
+      </Modal>
     </div>
   );
 };
